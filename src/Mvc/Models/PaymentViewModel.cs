098dbebcb7d551081f74aa0f42192b26aadcc99c
Maximiliano Raúl Malvicino <mrmalvicino@gmail.com>
2026-01-31 17:12:58 -0300

    feat: Agrega las vistas Create, Delete y Details y el ítem Pagos en la barra de menú [MEM-3]

--- src/Mvc/Models/PaymentViewModel.cs
+++ src/Mvc/Models/PaymentViewModel.cs
@@ -4,6 +4,7 @@ namespace Mvc.Models
 {
     public class PaymentViewModel
     {
+        [Display(Name = "ID")]
         public int Id { get; set; }
 
         [Display(Name = "Fecha de pago")]
@@ -11,10 +12,12 @@ namespace Mvc.Models
         public DateTime DateTime { get; set; } = DateTime.Now;
 
         [Display(Name = "Monto")]
-        [Required]
+        [Required(ErrorMessage = "El importe es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El importe debe ser mayor a 0")]
         public decimal Amount { get; set; }
 
         [Display(Name = "Socio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar un socio")]
         public int MemberId { get; set; }
 
         public virtual MemberViewModel? Member { get; set; }
