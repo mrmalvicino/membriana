c0493c3fae6e64bdd91c5b2df2dd2dbf68f6202e
Maximiliano Raúl Malvicino <mrmalvicino@gmail.com>
2026-02-24 23:49:27 -0300

    feat: Agrega clase que representa un registro de cambio de estado [MEM-10]

---
+++ src/Domain/Entities/MemberStatusEvent.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Enums;
+using Domain.Interfaces;
+
+namespace Domain.Entities;
+
+/// <summary>
+/// Representa un cambio de estado en la situación financiera de un miembro dentro
+/// de una organización. Incluye información sobre el estado anterior, el nuevo estado, la
+/// fecha y hora del cambio, el usuario que realizó el cambio y otros detalles relevantes.
+/// </summary>
+public class MemberStatusEvent : ITenantable, IAuditable
+{
+    #region Id
+    public int Id { get; set; }
+    #endregion
+
+    #region Member
+    public int MemberId { get; set; }
+    public Member Member { get; set; } = null!;
+    #endregion
+
+    #region Organization
+    public int OrganizationId { get; set; }
+    public Organization Organization { get; set; } = null!;
+    #endregion
+
+    #region PreviousStatus
+    public MemberStatus? PreviousStatus { get; set; }
+    #endregion
+
+    #region NewStatus
+    public MemberStatus NewStatus { get; set; }
+    #endregion
+
+    #region ChangedAtDateTime
+    public DateTime ChangedAtDateTime { get; set; }
+    #endregion
+    
+    #region ChangedByUser
+    public int ChangedByUserId { get; set; }
+    public AppUser ChangedByUser { get; set; } = null!;
+    #endregion
+
+    #region Details
+    public string? Details { get; set; }
+    #endregion
+}
---
+++ src/Domain/Interfaces/IAuditable.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    /// <summary>
+    /// Define la información de auditoría para las entidades que implementan esta interfaz.
+    /// </summary>
+    public interface IAuditable
+    {
+        DateTime ChangedAtDateTime { get; set; }
+        AppUser ChangedByUser { get; set; }
+        string? Details { get; set; }
+    }
+}
